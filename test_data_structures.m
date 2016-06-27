f6775ef799d66590efa94abfff4feec12fff176e
Nima Bigdely-Shamlo <nimabg@bitbucket.org>
2016-06-27 08:33:44 -0700

    updated obj and LARG tool tests

--- test_data_structures.m
+++ test_data_structures.m
@@ -1,9 +1,24 @@
+% defining Blocks
 b = Block('tensor', [1 2 3; 4 5 6], 'axes', {TimeAxis('times', [0 0.1]), FrequencyAxis('frequencies', [1 2 3])});
+assert(isequal(b({'frequency', 2}), [2     5]));
+%%
 b = Block('tensor', [1 2 3; 4 5 6], 'axes', {TimeAxis('times', [0 0.1]), TimeAxis('times', [1 2 3])});
 
 %%
+t = zeros(2, 3, 4);
+t(:) = 1:length(t(:));
+b = Block('tensor', t, 'axes', {TimeAxis('times', [0 0.1]), FrequencyAxis('frequencies', [1 2 3]), TrialAxis('times', [4 5 6 7])});
+
+%% slicing axis
 b = Block('tensor', [1 2 3; 4 5 6], 'axes', {TimeAxis('times', [0 0.1]), FrequencyAxis('frequencies', [1 2 3])});
-newObj = b.sliceAxes('time', 1, 'frequencies', 1:2);
+b({'time', 1}, :);
+b({'frequency', 1}, :);
+b({'time', 1:2}, {'frequency', 1});
+newObj = b.sliceAxes('frequency', 1, 'time', 1:2);
+assert(newObj.isValid);
+
+newObj2 = b.sliceAxes('time', 2);
+assert(newObj2.isValid);
 %%
 b = Block('tensor', [1 2 3; 4 5 6], 'axes', {TimeAxis('times', [0 0.1]), SpaceAxis('length', 3)});
 assert(isequal(b({'time', '2:2'}, :), [ 4 5 6]));
@@ -42,4 +57,16 @@ obj = EpochedTemporalFeature;
 obj = obj.epochChannels(EEG, 'select', {'maxSameTypeCount', 500});
 %% 
 load([fileparts(which('test_level1.m')) filesep 'for_feature' filesep 'RSVP_events_epochedTemporalFEature.mat']); % loads obj variable
+[newObj, cleanIds, averageOutlierRatio]= obj.removeNoisyEpochs('zthreshold', 5.5);
 
+[v id] = sort(averageOutlierRatio);
+figure; imagesc(obj.index({'trial', id}, 'time', 3));
+q = quantile(abs(vec(obj.index('trial', {'channel', 3}, :))), 0.98);
+caxis([-q q])
+%% Axis slicing
+t = TimeAxis('times', [0:0.1:10]);
+assert(length(t(1:5).times) == 5);
+
+% make sure multidimensional per-item axis properties slice only on the first dimension 
+c = ChannelAxis('length', 3, 'positions', rand(3, 5));
+assert(isequal(size(c(1:2).positions), [2     5]));
